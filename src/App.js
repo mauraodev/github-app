2e1ab051d840e747ce42baded7ccefead2f9ba03
Mauro Nunes <mauraoemau@hotmail.com>
2019-12-22 08:55:16 -0300

    Marcação

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='search'>
+        <input type='search' placeholder='Digite o nome do usuário no Github'/>
+      </div>
+
+      <div className='user-info'>
+        <img src='https://avatars0.githubusercontent.com/u/1053242?v=4'/>
+        <h1>
+          <a href='https://github.com/mauraodev'>Mauro Nunes</a>
+        </h1>
+
+        <ul className='repos-info'>
+          <li>Repositórios: 11</li>
+          <li>Seguirdores: 11</li>
+          <li>Seguindo: 11</li>
+        </ul>
+
+        <div className='actions'>
+          <button>Ver repositórios</button>
+          <button>Ver favotiros</button>
+        </div>
+
+        <div className='repos'>
+          <h2>Repositórios</h2>
+          <ul>
+            <li>
+              <a href='#'>Nome do repositório</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className='starred'>
+          <h2>Favoritos</h2>
+          <ul>
+            <li>
+              <a href='#'>Nome do repositório</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
