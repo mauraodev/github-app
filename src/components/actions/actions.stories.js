016621dd408ff91a63d85326cee2ea944e10a09f
Mauro Nunes <mauraoemau@hotmail.com>
2020-05-23 17:58:42 -0300

    Fix stories and files .lock

--- src/components/actions/actions.stories.js
+++ src/components/actions/actions.stories.js
@@ -1,9 +1,13 @@
-import { storiesOf } from '@storybook/react'  
-import { actions } from '@storybook/addon-actions';
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions';
 import React from 'react'
 import Actions from '../actions'
 
-storiesOf('Actions', module)
-    .add('first story', () => (
-        <Actions getRepos={actions('getRepos')} getStarred={actions('getStarred')}/>
-    ))
+const stories = storiesOf('Actions', module)
+
+stories.add('first story', () => (
+    <Actions getRepos={action('Get Repo')} getStarred={action('Get Starred')} />
+))
+stories.add('second story', () => (
+    <Actions getRepos={action('Get Repo')} getStarred={action('Get Starred')} />
+))
